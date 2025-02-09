9955d62f48a44c6965f853696888fa72c53b4e5b
a.abhinn <abhinn000000@gmail.com>
2025-02-09 18:28:31 +0530

    [Abhinn] Add. adding files

---
+++ OOPS_2/Employee.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Employee
+    {
+        //Attributes
+        private string Name { get; set; }
+        private int Id { get; set; }
+        private double Salary { get; set; }
+
+        //Constructor
+        public Employee(string name,int id,double salary) {
+            Name = name;
+            Id = id;
+            Salary = salary;
+        }
+
+        //Display details
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine("The name of the employees is " + Name);
+            Console.WriteLine("The Id of the employees is " + Id);
+            Console.WriteLine("The salary of the employees is " + Salary);
+        }
+    }
+
+    class Manager : Employee
+    {
+        //Constructor
+        private int TeamSize { get; set; }
+        public Manager(string Name, int Id, double Salary, int teamSize) : base(Name,Id,Salary) { //calling parent class constructor using base()
+            TeamSize = teamSize;
+        }
+
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine("The Team size of the Manager is "+ TeamSize);
+        }
+    }
+
+    class Developer : Employee {
+        //Attribute of Developer
+        private string ProgrammingLanguage { get; set; }
+
+        //Constructor
+        public Developer(string Name,int Id,double Salary,string programmingLanguage) : base(Name,Id,Salary) {
+            ProgrammingLanguage = programmingLanguage;
+        }
+
+        //Display Details method
+        public override void DisplayDetails()
+        {
+            base.DisplayDetails();
+            Console.WriteLine("The programming language of the developer is "+ProgrammingLanguage);
+        }
+    }
+
+    class Intern : Employee {
+        //Attribute
+        private string InternshipDuration { get; set; }
+
+        //Constructor
+        public Intern(string Name, int Id, double Salary, string internshipDuration) : base(Name, Id, Salary) {
+            InternshipDuration = internshipDuration;
+        }
+
+        //Method to display details of intern
+        public override void DisplayDetails() {
+            base.DisplayDetails();
+            Console.WriteLine("The internship duration for the intern is "+InternshipDuration);
+        }
+    }
+
+    class program2 {
+        //public static void Main(String[] args)
+        //{
+        //    Employee employee1 = new Employee("Ajeet", 1, 10000.20);
+        //    employee1.DisplayDetails();
+
+        //    Manager manager1 = new Manager("Aviral", 2, 100000.54, 7);
+        //    manager1.DisplayDetails();
+
+        //    Developer developer1 = new Developer("Takeshi", 3, 89000.87, "C#");
+        //    developer1.DisplayDetails();
+
+        //    Intern intern1 = new Intern("Bobby", 4, 20000, "6 months");
+        //    intern1.DisplayDetails();
+        //}
+    }
+}
---
+++ OOPS_2/Restaurant.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+//interface 
+interface Worker
+{
+    void PerformDuties();
+}
+
+class Person
+{
+    //Attributes
+    public string Name { get; set; }
+    public int Id { get; set; }
+
+    //Constructor
+    public Person(string name, int id)
+    {
+        Name = name;
+        Id = id;
+    }
+    //method to display details
+    public void ShowInfo()
+    {
+        Console.WriteLine("Name: " + Name + ", ID: " + Id);
+    }
+}
+
+
+class Chef : Person, Worker
+{
+    //Constructor
+    public Chef(string name, int id) : base(name, id) { }
+
+    //method implementation of the interface
+    public void PerformDuties()
+    {
+        Console.WriteLine(Name + " is preparing meals.");
+    }
+}
+
+class Waiter : Person, Worker
+{
+    //Constructor
+    public Waiter(string name, int id) : base(name, id) { }
+
+    //method implementation of the interface
+    public void PerformDuties()
+    {
+        Console.WriteLine(Name + " is serving customers.");
+    }
+}
+
+class program8
+{
+    //static void Main(string[] args)
+    //{
+    //    Chef chef = new Chef("Gajendra", 101);
+    //    Waiter waiter = new Waiter("Amitabh", 201);
+
+    //    chef.ShowInfo();
+    //    chef.PerformDuties();
+
+    //    waiter.ShowInfo();
+    //    waiter.PerformDuties();
+    //}
+}
